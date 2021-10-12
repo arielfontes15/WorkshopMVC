7585d1b8a1d5d9cdca9a80844f7e773f1c2d0c5c
Ariel <arielfontes15@hotmail.com>
2021-10-12 18:37:05 -0300

    Crud Scaffolding

---
+++ WorkshopMVC/Data/WorkshopMVCContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WorkshopMVC.Models;
+
+namespace WorkshopMVC.Data
+{
+    public class WorkshopMVCContext : DbContext
+    {
+        public WorkshopMVCContext (DbContextOptions<WorkshopMVCContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<WorkshopMVC.Models.Departamento> Departamento { get; set; }
+    }
+}
