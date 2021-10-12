5533834773f30b7a655276e61378516fe4b8ddcc
Ariel <arielfontes15@hotmail.com>
2021-10-12 14:13:16 -0300

    subindo arquivos

---
+++ WorkshopMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WorkshopMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
